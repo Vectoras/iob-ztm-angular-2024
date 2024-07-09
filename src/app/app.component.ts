b9b5bac82cb15484d9a36b1ec60512bc5ab1aab7
Vectoras <johnny.boghean.developer@gmail.com>
2024-07-09 20:07:45 +0100

    Component Fundamentals - app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ztm-angular';
+  name = 'Luis';
+  imageUrl = 'https://picsum.photos/id/237/500/500'
+
+  getName() {
+    return this.name;
+  }
+
+  changeImage (e: KeyboardEvent) {
+    this.imageUrl=(e.target as HTMLInputElement).value;
+  }
 }
