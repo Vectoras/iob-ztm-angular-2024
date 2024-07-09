81a9fc4387e170a3c775baeb3513191f06b8780c
Vectoras <johnny.boghean.developer@gmail.com>
2024-07-09 21:01:22 +0100

    Component Fundamentals - hooks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,7 +20,53 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input('img') postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('[app-post] constructor()', this.postImg);
+  }
+
+  ngOnInit() {
+    console.log('[app-post] ngonInit()', this.postImg);
+  }
+
+  ngOnChanges() {
+    console.log('[app-post] ngonChagngOnChanges()', this.postImg);
+  }
+
+  ngDoCheck() {
+    console.log('[app-post] ngonChagngDoCheck()', this.postImg);
+  }
+
+  ngAfterContentInit() {
+    console.log('[app-post] ngonChagngAfterContentInit()', this.postImg);
+  }
+
+  ngAfterContentChecked() {
+    console.log('[app-post] ngonChagngAfterContentChecked()', this.postImg);
+  }
+
+  ngAfterViewInit() {
+    console.log('[app-post] ngonChagngAfterViewInit()', this.postImg);
+  }
+
+  ngAfterViewChecked() {
+    console.log('[app-post] ngonChagngAfterViewChecked()', this.postImg);
+  }
+
+  ngOnDestroy() {
+    console.log('[app-post] ngonChagngOnDestroy()', this.postImg);
+  }
 }
