3f90a09f6c4ccf107b7167a5a3e4f2795e2aad13
Vectoras <johnny.boghean.developer@gmail.com>
2024-09-21 07:22:52 +0100

    Master Project: Reactive forms - form validation

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+})
+export class InputComponent implements OnInit {
+  @Input() type: 'text' | 'email' | 'number' | 'password' = 'text';
+  @Input() label = '';
+  @Input() placeholder = '';
+  @Input() control = new FormControl();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +8,15 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl([]),
-    email: new FormControl([]),
-    age: new FormControl([]),
-    password: new FormControl([]),
-    confirm_password: new FormControl([]),
-    phoneNumber: new FormControl([]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120),
+    ]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).*)/)]),
+    confirm_password: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', []),
   });
 }
